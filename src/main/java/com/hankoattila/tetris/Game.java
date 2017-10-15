0848aa376148d0b2a15c2f5cc83d8a01a487a4bb
hankoattila <hattilla4@gmail.com>
2017-10-15 15:54:53 +0200

    Created a basic methods

---
+++ src/main/java/com/hankoattila/tetris/Game.java
@@ -0,0 +1,10 @@
+package com.hankoattila.tetris;
+
+import javafx.scene.layout.Pane;
+
+public class Game extends Pane {
+
+    public void start(){
+
+    }
+}
---
+++ src/main/java/com/hankoattila/tetris/GameEntity.java
@@ -0,0 +1,27 @@
+package com.hankoattila.tetris;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+public abstract class GameEntity extends ImageView {
+    protected Pane pane;
+
+    protected GameEntity(Pane pane) {
+        this.pane = pane;
+        // add to the main loop.
+        Globals.addGameObject(this);
+    }
+
+    public void destroy() {
+        if (getParent() != null) {
+            pane.getChildren().remove(this);
+        }
+        Globals.removeGameObject(this);
+    }
+
+    protected boolean isOutOfBounds() {
+        return false;
+    }
+
+
+}
