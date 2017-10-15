36610ec1d38e8e079c8a688c4f73003000c20af8
hankoattila <hattilla4@gmail.com>
2017-10-15 15:38:11 +0200

    Animatable class will call the step method 60 times in every second

---
+++ src/main/java/com/hankoattila/tetris/Animatable.java
@@ -0,0 +1,9 @@
+package com.hankoattila.tetris;
+
+
+// Interface for animated game entities. If a GameEntity implements this, the step() method will be called
+// 60 times per second.
+public interface Animatable {
+
+    void step();
+}
