95e6a7c39526b3fedacfd3bec07d388f982a9853
hankoattila <hattilla4@gmail.com>
2017-10-15 15:53:30 +0200

    Created a createStartwindow skeleton

---
+++ src/main/java/com/hankoattila/tetris/Gui.java
@@ -0,0 +1,10 @@
+package com.hankoattila.tetris;
+
+import javafx.stage.*;
+
+public class Gui {
+    public static Stage createStartWindow() {
+        Stage initialise = new Stage();
+        return initialise;
+    }
+}
