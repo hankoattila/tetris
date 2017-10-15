1bbd3076504f86d04f39367c5b40dacc047c5af5
hankoattila <hattilla4@gmail.com>
2017-10-15 17:35:23 +0200

    When the game is start, then add a new object to pane

--- src/main/java/com/hankoattila/tetris/Game.java
+++ src/main/java/com/hankoattila/tetris/Game.java
@@ -4,7 +4,11 @@ import javafx.scene.layout.Pane;
 
 public class Game extends Pane {
 
-    public void start(){
-
+    public void start() {
+        Square square = new Square(this, 200, 200);
+        Globals.pane = this;
+        GameLoop gameLoop = new GameLoop();
+        Globals.gameLoop = gameLoop;
+        Globals.gameLoop.start();
     }
 }
