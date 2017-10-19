1bb70c5f547ee5d4fb4c50d05713136023e64aea
hankoattila <hattilla4@gmail.com>
2017-10-19 22:32:20 +0200

    BodyBlock refactored, and in apply that call his parent apply

--- src/main/java/com/hankoattila/tetris/elements/BodyBlock.java
+++ src/main/java/com/hankoattila/tetris/elements/BodyBlock.java
@@ -1,28 +1,32 @@
 package com.hankoattila.tetris.elements;
 
-import com.hankoattila.tetris.*;
+import com.hankoattila.tetris.Animatable;
+import com.hankoattila.tetris.GameEntity;
+import com.hankoattila.tetris.Globals;
+import com.hankoattila.tetris.Interactable;
 import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
 public class BodyBlock extends Animatable implements Interactable {
-    int speed;
+    GameEntity parent;
 
-    protected BodyBlock(Pane pane, double xc, double yc) {
+    protected BodyBlock(Pane pane, double xc, double yc, GameEntity parent) {
         super(pane);
         this.pane = pane;
-        speed = 4;
+        this.parent = parent;
         setX(xc);
         setY(yc);
 
         pane.getChildren().add(this);
         setImage(new Image("square.png"));
     }
+
     public void step() {
         setY(getY() + Globals.BLOCK_SIZE);
     }
 
-    public void apply(GameEntity block) {
-
+    public void apply() {
+        ((Interactable)parent).apply();
     }
 
     public String getMessage() {
