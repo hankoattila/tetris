2db6b3feb85c217827da5fa10c336cd3fe393526
hankoattila <hattilla4@gmail.com>
2017-10-19 14:31:20 +0200

    The feature blocks are in comment

--- src/main/java/com/hankoattila/tetris/Blocks.java
+++ src/main/java/com/hankoattila/tetris/Blocks.java
@@ -22,11 +22,11 @@ public enum   Blocks {
         switch (number){
             case 0:return new IBlock(pane,200,0);
             case 1:return new JBlock(pane,200,0);
-            case 2:return new LBlock(pane,200,0);
-            case 3:return new OBlock(pane,200,0);
-            case 4:return new SBlock(pane,200,0);
-            case 5:return new TBlock(pane,200,0);
-            case 6:return new ZBlock(pane,200,0);
+//            case 2:return new LBlock(pane,200,0);
+//            case 3:return new OBlock(pane,200,0);
+//            case 4:return new SBlock(pane,200,0);
+//            case 5:return new TBlock(pane,200,0);
+//            case 6:return new ZBlock(pane,200,0);
 
         }
         return null;
