cb0445052872e6299d59e8f884b01389f01377f6
hankoattila <hattilla4@gmail.com>
2017-10-19 22:41:11 +0200

    Basic LBlock constructor implemented

--- src/main/java/com/hankoattila/tetris/elements/LBlock.java
+++ src/main/java/com/hankoattila/tetris/elements/LBlock.java
@@ -1,9 +1,23 @@
 package com.hankoattila.tetris.elements;
 
+import com.hankoattila.tetris.Animatable;
+import com.hankoattila.tetris.Globals;
+import com.hankoattila.tetris.Interactable;
+import javafx.scene.image.Image;
 import javafx.scene.layout.Pane;
 
-public class LBlock  {
+public class LBlock extends Animatable implements Interactable {
     public LBlock(Pane pane, int x, int y) {
+        super(pane);
+        this.pane = pane;
+        setX(x);
+        setY(y);
+        blockList.add(this);
+        blockList.add(new BodyBlock(pane, x + Globals.BLOCK_SIZE, y, this));
+        blockList.add(new BodyBlock(pane, x, y - Globals.BLOCK_SIZE, this));
+        blockList.add(new BodyBlock(pane, x, y - Globals.BLOCK_SIZE * 2, this));
+        setImage(new Image("square.png"));
+        pane.getChildren().add(this);
 
     }
 
