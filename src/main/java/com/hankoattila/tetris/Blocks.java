72b612af23def0bd68d0219ccaf6bbed12b69afa
hankoattila <hattilla4@gmail.com>
2017-10-19 22:58:52 +0200

    Blocks creation is randomly

--- src/main/java/com/hankoattila/tetris/Blocks.java
+++ src/main/java/com/hankoattila/tetris/Blocks.java
@@ -3,6 +3,8 @@ package com.hankoattila.tetris;
 import com.hankoattila.tetris.elements.*;
 import javafx.scene.layout.Pane;
 
+import java.util.Random;
+
 public enum   Blocks {
     IBLOCK("iblock"),
     JBLOCK("jblock"),
@@ -19,14 +21,17 @@ public enum   Blocks {
     }
 
     public static GameEntity createNewBlock(int number ,Pane pane){
+        Random rnd = new Random();
+        int z = rnd.nextInt(5);
+        int x = 100 + (z * Globals.BLOCK_SIZE);
         switch (number){
-            case 0:return new IBlock(pane,200-Globals.BLOCK_SIZE,0);
-            case 1:return new JBlock(pane,200+Globals.BLOCK_SIZE,0);
-            case 2:return new LBlock(pane,200,0);
-//            case 3:return new OBlock(pane,200,0);
+            case 0:return new IBlock(pane,x,0);
+            case 1:return new JBlock(pane,x,0);
+            case 2:return new LBlock(pane,x,0);
+            case 3:return new OBlock(pane,x,0);
 //            case 4:return new SBlock(pane,200,0);
 //            case 5:return new TBlock(pane,200,0);
-            case 3:return new ZBlock(pane,200,0);
+            case 4:return new ZBlock(pane,x,0);
 
         }
         return null;
