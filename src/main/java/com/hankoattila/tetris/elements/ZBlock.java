3f122df545185964ff08a0be37a603676175a0c7
hankoattila <hattilla4@gmail.com>
2017-10-22 20:29:20 +0200

    Basic ZBlock rotate implemented

--- src/main/java/com/hankoattila/tetris/elements/ZBlock.java
+++ src/main/java/com/hankoattila/tetris/elements/ZBlock.java
@@ -1,6 +1,7 @@
 package com.hankoattila.tetris.elements;
 
 import com.hankoattila.tetris.Animatable;
+import com.hankoattila.tetris.GameEntity;
 import com.hankoattila.tetris.Globals;
 import com.hankoattila.tetris.Interactable;
 import javafx.scene.image.Image;
@@ -13,12 +14,41 @@ public class ZBlock extends Animatable implements Interactable {
         this.pane = pane;
         setX(x);
         setY(y);
+        blockList.add(this);
         blockList.add(new BodyBlock(pane, x, y - Globals.BLOCK_SIZE,image, this));
         blockList.add(new BodyBlock(pane, x + Globals.BLOCK_SIZE, y,image, this));
         blockList.add(new BodyBlock(pane, x - Globals.BLOCK_SIZE, y - Globals.BLOCK_SIZE, image,this));
-        blockList.add(this);
-        setImage(new Image(this.image));
+        setImage(new Image("square_pink.png"));
         pane.getChildren().add(this);
 
+    }
+
+    @Override
+    protected void switchPositions() {
+        int Px = (int) blockList.get(0).getX();
+        int Py = (int) blockList.get(0).getY();
+
+        for (GameEntity block : blockList) {
+
+            int Vx = (int) block.getX();
+            int Vy = (int) block.getY();
+
+            int Vrx = Vx - Px;
+            int Vry = Vy - Py;
+
+            int Rx1 = 0;
+            int Ry1 = 1;
+            int Rx2 = -1;
+            int Ry2 = 0;
+
+
+
+            int Vtx = Rx1 * Vrx + Rx2 * Vry;
+            int Vty = Ry1 * Vrx + Ry2 * Vry;
+            int V1 = Px + Vtx;
+            int V2 = Py + Vty;
+            block.setX(V1);
+            block.setY(V2);
+        }
     }
 }
