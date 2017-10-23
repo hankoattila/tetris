40cc1a92384e714bd671c79a1b0f00d961cef40b
hankoattila <hattilla4@gmail.com>
2017-10-23 10:53:16 +0200

    BlockDown renamed to TableBlock

--- src/main/java/com/hankoattila/tetris/Game.java
+++ src/main/java/com/hankoattila/tetris/Game.java
@@ -1,5 +1,7 @@
 package com.hankoattila.tetris;
 
+import com.hankoattila.tetris.entities.BlockTypes;
+import com.hankoattila.tetris.entities.TableBlock;
 import javafx.scene.layout.Pane;
 
 import java.util.Random;
@@ -11,24 +13,24 @@ public class Game extends Pane {
         createBlockList();
         for (int i = 0; i < Globals.WINDOW_WIDTH; i += Globals.BLOCK_SIZE) {
             for (int j = 0; j < Globals.WINDOW_HEIGHT; j += Globals.BLOCK_SIZE) {
-                new BlockDown(this, i, j, "bcg.jpg");
+                new TableBlock(this, i, j, "bcg.jpg");
             }
 
         }
-        Blocks.createNewBlock(rnd.nextInt(Globals.blocks.size()), this);
+        BlockTypes.createNewBlock(rnd.nextInt(Globals.blocks.size()), this);
         GameLoop gameLoop = new GameLoop();
         Globals.gameLoop = gameLoop;
         Globals.gameLoop.start();
     }
 
     private void createBlockList() {
-        Globals.blocks.add(Blocks.IBLOCK);
-        Globals.blocks.add(Blocks.JBLOCK);
-        Globals.blocks.add(Blocks.LBLOCK);
-        Globals.blocks.add(Blocks.OBLOCK);
-        Globals.blocks.add(Blocks.SBLOCK);
-        Globals.blocks.add(Blocks.TBLOCK);
-        Globals.blocks.add(Blocks.ZBLOCK);
+        Globals.blocks.add(BlockTypes.IBLOCK);
+        Globals.blocks.add(BlockTypes.JBLOCK);
+        Globals.blocks.add(BlockTypes.LBLOCK);
+        Globals.blocks.add(BlockTypes.OBLOCK);
+        Globals.blocks.add(BlockTypes.SBLOCK);
+        Globals.blocks.add(BlockTypes.TBLOCK);
+        Globals.blocks.add(BlockTypes.ZBLOCK);
 
 
     }
