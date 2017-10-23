56cad6016d55c7b4e4518142d71d985d69a9e5ff
hankoattila <hattilla4@gmail.com>
2017-10-23 10:49:12 +0200

    Block class implement all method what the block entities have, all block type extends this class

--- src/main/java/com/hankoattila/tetris/entities/Block.java
+++ src/main/java/com/hankoattila/tetris/entities/Block.java
@@ -1,6 +1,6 @@
-package com.hankoattila.tetris;
+package com.hankoattila.tetris.entities;
 
-
+import com.hankoattila.tetris.Globals;
 import javafx.event.EventHandler;
 import javafx.geometry.Point2D;
 import javafx.scene.input.KeyCode;
@@ -10,37 +10,33 @@ import javafx.scene.layout.Pane;
 import java.util.Collections;
 import java.util.Random;
 
-// Interface for animated game entities. If a GameEntity implements this, the step() method will be called
-// 60 times per second.
-public abstract class Animatable extends GameEntity implements Interactable {
-    protected boolean leftKeyDown = false;
-    protected boolean rightKeyDown = false;
-    protected boolean downKeyDown = false;
-    protected boolean switchKey = false;
+public class Block extends GameEntity implements Interactable, Animatable {
+    private boolean leftKeyDown = false;
+    private boolean rightKeyDown = false;
+    private boolean downKeyDown = false;
+    private boolean switchKey = false;
     protected String image;
 
-    protected Animatable(Pane pane, String image) {
+
+    protected Block(Pane pane, String image) {
         super(pane);
         this.image = image;
         initEventHandlers(pane, KeyCode.LEFT, KeyCode.RIGHT, KeyCode.DOWN, KeyCode.UP);
 
     }
 
-    public void step() {
+    public void changePosition() {
         Globals.speed = Globals.speedLimit;
         if (switchKey) {
-            if (blockList.size() >0) {
+            if (blockList.size() > 0) {
                 switchPositions();
-                move();
             }
-
         } else if (leftKeyDown) {
             if (!outOfLeftBound() && isEmptyLeftPosition()) {
                 for (GameEntity gameEntity : blockList) {
                     gameEntity.setX(gameEntity.getX() - Globals.BLOCK_SIZE);
                 }
             }
-            move();
         } else if (rightKeyDown) {
             if (!outOfRightBound() && isEmptyRightPosition()) {
 
@@ -48,17 +44,17 @@ public abstract class Animatable extends GameEntity implements Interactable {
                     gameEntity.setX(gameEntity.getX() + Globals.BLOCK_SIZE);
                 }
             }
-            move();
-
         } else if (downKeyDown) {
             Globals.speed = Globals.speedBoost;
-            move();
-        } else {
-            move();
         }
+
     }
 
-    protected abstract void switchPositions();
+    public void step() {
+        for (GameEntity gameEntity : blockList) {
+            gameEntity.setY(gameEntity.getY() + Globals.BLOCK_SIZE);
+        }
+    }
 
     public void apply() {
         if (blockList.size() != 0) {
@@ -68,7 +64,7 @@ public abstract class Animatable extends GameEntity implements Interactable {
                 int x = (int) blockList.get(0).getX();
                 int y = (int) blockList.get(0).getY();
                 Globals.positions.put(new Point2D(x, y), image);
-                new BlockDown(pane, x, y, this.image);
+                new TableBlock(pane, x, y, this.image);
                 blockList.get(0).destroy();
                 blockList.remove(0);
             }
@@ -78,7 +74,34 @@ public abstract class Animatable extends GameEntity implements Interactable {
                 setBlockNewPosition();
             }
             Random rnd = new Random();
-            Blocks.createNewBlock(rnd.nextInt(Globals.blocks.size()), pane);
+            BlockTypes.createNewBlock(rnd.nextInt(Globals.blocks.size()), pane);
+        }
+    }
+
+    protected void switchPositions() {
+        int Px = (int) blockList.get(0).getX();
+        int Py = (int) blockList.get(0).getY();
+
+        for (GameEntity block : blockList) {
+
+            int Vx = (int) block.getX();
+            int Vy = (int) block.getY();
+
+            int Vrx = Vx - Px;
+            int Vry = Vy - Py;
+
+            int Rx1 = 0;
+            int Ry1 = 1;
+            int Rx2 = -1;
+            int Ry2 = 0;
+
+
+            int Vtx = Rx1 * Vrx + Rx2 * Vry;
+            int Vty = Ry1 * Vrx + Ry2 * Vry;
+            int V1 = Px + Vtx;
+            int V2 = Py + Vty;
+            block.setX(V1);
+            block.setY(V2);
         }
     }
 
@@ -102,18 +125,18 @@ public abstract class Animatable extends GameEntity implements Interactable {
         for (int y : Globals.removeLine) {
             for (int x = 0; x < Globals.WINDOW_WIDTH; x += Globals.BLOCK_SIZE) {
                 Globals.positions.remove(new Point2D(x, y));
-                new BlockDown(pane, x, y, "bcg.jpg");
+                new TableBlock(pane, x, y, "bcg.jpg");
             }
         }
         int start = Collections.max(Globals.removeLine) - Globals.BLOCK_SIZE;
         for (int y = start; y > 0; y -= Globals.BLOCK_SIZE) {
             for (int x = 0; x < Globals.WINDOW_HEIGHT; x += Globals.BLOCK_SIZE) {
                 if (Globals.positions.containsKey(new Point2D(x, y))) {
-                    new BlockDown(pane, x, y, "bcg.jpg");
+                    new TableBlock(pane, x, y, "bcg.jpg");
                     String picture = Globals.positions.get(new Point2D(x, y));
                     Globals.positions.remove(new Point2D(x, y));
                     Globals.positions.put(new Point2D(x, y + Globals.BLOCK_SIZE * Globals.removeLine.size()), picture);
-                    new BlockDown(pane, x, y + Globals.BLOCK_SIZE * Globals.removeLine.size(), picture);
+                    new TableBlock(pane, x, y + Globals.BLOCK_SIZE * Globals.removeLine.size(), picture);
                 }
             }
 
@@ -121,8 +144,12 @@ public abstract class Animatable extends GameEntity implements Interactable {
 
     }
 
-    protected void initEventHandlers(Pane pane, final KeyCode leftCode, final KeyCode rightCode,
-                                     final KeyCode downCode, final KeyCode switchCode) {
+    protected void initEventHandlers(Pane pane,
+                                     final KeyCode leftCode,
+                                     final KeyCode rightCode,
+                                     final KeyCode downCode,
+                                     final KeyCode switchCode) {
+
         //EventHandler has to be chained to each other due to only one can be set
         final EventHandler oldKeyPressedHandler = pane.getScene().getOnKeyPressed();
         pane.getScene().setOnKeyPressed(new EventHandler<KeyEvent>() {
