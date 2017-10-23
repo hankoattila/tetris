c27f9e498f64e7b6aa58c6c731df22c5a8872037
hankoattila <hattilla4@gmail.com>
2017-10-23 10:50:33 +0200

    The game speed changed

--- src/main/java/com/hankoattila/tetris/GameLoop.java
+++ src/main/java/com/hankoattila/tetris/GameLoop.java
@@ -1,35 +1,57 @@
 package com.hankoattila.tetris;
 
+import com.hankoattila.tetris.entities.Animatable;
+import com.hankoattila.tetris.entities.Block;
+import com.hankoattila.tetris.entities.GameEntity;
+import com.hankoattila.tetris.entities.Interactable;
 import javafx.animation.AnimationTimer;
 
 
 public class GameLoop extends AnimationTimer {
-    private int tetrisStep = 0;
+    private int tetrisSpeed = 0;
+    private int rotation = 0;
 
     public void handle(long now) {
-        tetrisStep++;
-        if (tetrisStep == Globals.speed) {
-            //Call the apply method on every Interactable gameObject, before the step if condition is true
-            for (GameEntity gameObject : Globals.gameObjects) {
-                if (gameObject.isOutOfBottomBound() || gameObject.isObjectUnder()) {
-                    ((Interactable) gameObject).apply();
-                }
+        tetrisSpeed++;
+        rotation++;
+        
+        for (GameEntity gameObject : Globals.gameObjects) {
+            if (gameObject.isOutOfBottomBound() || gameObject.isObjectUnder()) {
+                ((Interactable) gameObject).apply();
             }
+        }
+        if (rotation == 40){
+
             for (GameEntity gameObject : Globals.gameObjects) {
                 if (gameObject instanceof Animatable) {
                     Animatable animObject = (Animatable) gameObject;
+                    if (gameObject instanceof Block) {
+                        ((Block) gameObject).changePosition();
+                    }
+                }
+            }
+            rotation = 0;
+        }
+
+        if (tetrisSpeed == Globals.speed) {
+
+            for (GameEntity gameObject : Globals.gameObjects) {
+                if (gameObject instanceof Animatable) {
+                    Animatable animObject = (Animatable) gameObject;
+                    if (gameObject instanceof Block) {
+                        ((Block) gameObject).changePosition();
+                    }
                     animObject.step();
                 }
             }
 
-
-            tetrisStep = 0;
-            Globals.gameObjects.addAll(Globals.newGameObjects);
-            Globals.newGameObjects.clear();
-
-            Globals.gameObjects.removeAll(Globals.oldGameObjects);
-            Globals.oldGameObjects.clear();
+            tetrisSpeed = 0;
         }
+        Globals.gameObjects.addAll(Globals.newGameObjects);
+        Globals.newGameObjects.clear();
+
+        Globals.gameObjects.removeAll(Globals.oldGameObjects);
+        Globals.oldGameObjects.clear();
 
     }
 }
--- src/main/java/com/hankoattila/tetris/Globals.java
+++ src/main/java/com/hankoattila/tetris/Globals.java
@@ -1,5 +1,6 @@
 package com.hankoattila.tetris;
 
+import com.hankoattila.tetris.entities.GameEntity;
 import javafx.geometry.Point2D;
 
 import java.util.ArrayList;
@@ -8,22 +9,23 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Globals {
-    public static int speed = 10;
+
+    public static int speedBoost =  5;
     public static int speedLimit = 10;
-    public static int speedBoost = 5;
-    public static final int BLOCK_SIZE = 31;
+    public static int speed = 10;
     public static int WIDTH = 12;
     public static int HEIGHT = 20;
-    public static List<Integer> removeLine = new ArrayList<>();
+    public static final int BLOCK_SIZE = 31;
     public static final int WINDOW_WIDTH = BLOCK_SIZE * WIDTH;
     public static final int WINDOW_HEIGHT = BLOCK_SIZE * HEIGHT;
     public static final int END_OF_WINDOW = WINDOW_HEIGHT - BLOCK_SIZE;
+    public static List<Enum> blocks = new ArrayList<Enum>();
+    public static List<Integer> removeLine = new ArrayList<>();
     public static HashMap<Point2D, String> positions = new HashMap<>();
     public static List<GameEntity> gameObjects = new ArrayList<GameEntity>(); //Holds game object that should be on display.
     public static List<GameEntity> newGameObjects = new ArrayList<GameEntity>(); // Holds game objects crated in this frame.
     public static List<GameEntity> oldGameObjects = new ArrayList<GameEntity>(); // Holds game objects that will be destroyed this frame.
     public static GameLoop gameLoop;
-    public static List<Enum> blocks = new ArrayList<Enum>();
 
     public static void removeGameObject(GameEntity toRemove) {
         oldGameObjects.add(toRemove);
