3d7e1c620fdcc4d0103d4720c82b99f95c6b4a3c
hankoattila <hattilla4@gmail.com>
2017-10-25 13:57:42 +0200

    Set a background colot in the start method

--- src/main/java/com/hankoattila/tetris/Game.java
+++ src/main/java/com/hankoattila/tetris/Game.java
@@ -2,8 +2,11 @@ package com.hankoattila.tetris;
 
 import com.hankoattila.tetris.entities.BlockTypes;
 import com.hankoattila.tetris.entities.TableBlock;
-import javafx.scene.layout.Pane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class Game extends Pane {
@@ -11,12 +14,25 @@ public class Game extends Pane {
     public void start() {
         Random rnd = new Random();
         createBlockList();
+        List<TableBlock> tableBlockList = new ArrayList<>();
         for (int i = 0; i < Globals.WINDOW_WIDTH; i += Globals.BLOCK_SIZE) {
             for (int j = 0; j < Globals.WINDOW_HEIGHT; j += Globals.BLOCK_SIZE) {
                 new TableBlock(this, i, j, "bcg.jpg");
             }
 
         }
+
+
+        // new Image(url)
+        Image image = new Image("wallpaper.jpg");
+        // new BackgroundSize(width, height, widthAsPercentage, heightAsPercentage, contain, cover)
+        BackgroundSize backgroundSize = new BackgroundSize(100, 100, true, true, true, false);
+        // new BackgroundImage(image, repeatX, repeatY, position, size)
+        BackgroundImage backgroundImage = new BackgroundImage(image, BackgroundRepeat.REPEAT, BackgroundRepeat.REPEAT, BackgroundPosition.CENTER, backgroundSize);
+        // new Background(images...)
+        Background background = new Background(backgroundImage);
+        this.setBackground(background);
+
         BlockTypes.createNewBlock(rnd.nextInt(Globals.blocks.size()), this);
         GameLoop gameLoop = new GameLoop();
         Globals.gameLoop = gameLoop;
